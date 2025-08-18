7c9b143905a7c51d3d6603cab5662608898b5c46
Victor Amadeu Braga Heleno <127600481+VictorAmadeu@users.noreply.github.com>
2025-08-18 23:20:06 +0200

    feature: init Angular app + Bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { AboutComponent } from './pages/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'articulos', component: ArticlesComponent },
+  { path: 'categorias', component: CategoriesComponent },
+  { path: 'sobre-mi', component: AboutComponent },
+  { path: '**', redirectTo: '' },
+];
