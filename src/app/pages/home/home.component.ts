386dcf02fce94b065201412811678185a40d1866
Victor Amadeu Braga Heleno <127600481+VictorAmadeu@users.noreply.github.com>
2025-08-21 11:04:25 +0200

    Primera actualización del proyecto en la Etapa 2: Estructura del proyecto Angular

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink], // Necesario para routerLink y [queryParams]
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
